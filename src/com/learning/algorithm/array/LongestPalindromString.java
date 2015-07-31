2471dfd6425ab0e6928b6872a0b61aeb530c6c02
prasenjit <prasenjuetce@gmail.com>
2015-07-31 22:10:34 +0530

    LCS based approach does not work. Removed it.

--- src/com/learning/algorithm/array/LongestPalindromString.java
+++ src/com/learning/algorithm/array/LongestPalindromString.java
@@ -7,47 +7,7 @@ public class LongestPalindromString {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		// StringBuilder str = new StringBuilder(sc.nextLine());
-
-		// StringBuilder rev = new StringBuilder(str).reverse();
-
-		// printLCS(str, rev);
-
 		printLCP(sc.nextLine());
-	}
-
-	// LCS based approach
-	private static void printLCS(StringBuilder str1, StringBuilder str2) {
-		int m = str1.length();
-		int n = str2.length();
-
-		// longest suff length for all pair of substrings
-		int[][] LCS = new int[m + 1][n + 1];
-		int maxLength = Integer.MIN_VALUE;
-		String result = "";
-
-		for (int i = 0; i <= m; i++) {
-			for (int j = 0; j <= n; j++) {
-				if (i == 0 || j == 0) {
-					LCS[i][j] = 0;
-					continue;
-				}
-
-				if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-					LCS[i][j] = LCS[i - 1][j - 1] + 1;
-				} else {
-					LCS[i][j] = 0;
-				}
-
-				// store max suffix length
-				if (maxLength < LCS[i][j]) {
-					maxLength = LCS[i][j];
-					result = str1.substring(i - maxLength, i);
-				}
-			}
-		}
-
-		System.out.println(result);
 	}
 
 	private static void printLCP(String str) {
